244b9abf41e5a4463811966089cdfb8c58ba3b93
Anshul Kanwar <4n5hu1@protonmail.com>
2022-07-30 20:55:38 +0530

    Updated Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="pt-12 p-8 text-slate-500 backdrop-blur-lg bg-white/75 w-full fixed top-0 left-0 z-50">
+    <div className="pt-12 p-8 text-slate-500 font-bold bg-white">
       <div className="flex justify-evenly">
-        <span className="hover:underline">
+        <span className="px-5 py-2 text-blue-500 bg-blue-100 rounded-sm skew-y-3 hover:skew-y-[-3]">
           <Link href="#about">About</Link>
         </span>
-        <span className="hover:underline">
+        <span className="px-5 py-2 text-emerald-500 bg-emerald-100 rounded-sm skew-y-3 hover:skew-y-[-3]">
           <Link href="#skills">Skills</Link>
         </span>
-        <span className="hover:underline">
+        <span className="px-5 py-2 text-fuchsia-500 bg-fuchsia-100 rounded-sm skew-y-3 hover:skew-y-[-3]">
           <Link href="#projects">Projects</Link>
         </span>
-        <span className="hover:underline">
+        <span className="px-5 py-2 text-red-500 bg-red-100 rounded-sm skew-y-3 hover:skew-y-[-3]">
           <Link href="#contact">Contact</Link>
         </span>
       </div>
