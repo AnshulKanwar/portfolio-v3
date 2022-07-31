ce30a3573f8b00c988290d3679aff7df7cf85ed5
Anshul Kanwar <4n5hu1@protonmail.com>
2022-07-31 11:56:23 +0530

    Updated Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -4,18 +4,26 @@ const Navbar = () => {
   return (
     <div className="pt-12 p-8 text-slate-500 font-bold bg-white">
       <div className="flex justify-evenly">
-        <span className="px-5 py-2 text-blue-500 bg-blue-100 rounded-sm skew-y-3 hover:skew-y-[-3]">
-          <Link href="#about">About</Link>
-        </span>
-        <span className="px-5 py-2 text-emerald-500 bg-emerald-100 rounded-sm skew-y-3 hover:skew-y-[-3]">
-          <Link href="#skills">Skills</Link>
-        </span>
-        <span className="px-5 py-2 text-fuchsia-500 bg-fuchsia-100 rounded-sm skew-y-3 hover:skew-y-[-3]">
-          <Link href="#projects">Projects</Link>
-        </span>
-        <span className="px-5 py-2 text-red-500 bg-red-100 rounded-sm skew-y-3 hover:skew-y-[-3]">
-          <Link href="#contact">Contact</Link>
-        </span>
+        <Link href="#about">
+          <span className="px-5 py-2 text-blue-500 bg-blue-100 rounded-sm skew-y-6 hover:transform-none transition">
+            About
+          </span>
+        </Link>
+        <Link href="#skills">
+          <span className="px-5 py-2 text-emerald-500 bg-emerald-100 rounded-sm skew-y-6 hover:transform-none transition">
+            Skills
+          </span>
+        </Link>
+        <Link href="#projects">
+          <span className="px-5 py-2 text-fuchsia-500 bg-fuchsia-100 rounded-sm skew-y-6 hover:transform-none transition">
+            Projects
+          </span>
+        </Link>
+        <Link href="#contact">
+          <span className="px-5 py-2 text-red-500 bg-red-100 rounded-sm skew-y-6 hover:transform-none transition">
+            Contact
+          </span>
+        </Link>
       </div>
     </div>
   );
