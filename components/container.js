cc61e9b4524b9729a2daf57bc70dbbb020bd67cb
Anshul Kanwar <4n5hu1@protonmail.com>
2022-07-31 11:27:17 +0530

    Update Font

--- components/container.js
+++ components/container.js
@@ -2,7 +2,9 @@ const Container = ({ name, fg, bg, children }) => {
   return (
     <div
       id={name.toLowerCase()}
-      className={`${bg || "bg-zinc-200"} ${fg} px-10 py-14 mx-20 my-5 rounded-3xl`}
+      className={`${
+        bg || "bg-zinc-200"
+      } ${fg} px-10 py-14 mx-20 my-5 rounded-3xl`}
     >
       <h1 className="text-center font-black text-5xl mb-8">{name}</h1>
       {children}
