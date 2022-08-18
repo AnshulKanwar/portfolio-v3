ca0edd7a11e93c502bf90b82337afaeb97bd74c1
Anshul Kanwar <4n5hu1@protonmail.com>
2022-08-18 19:55:54 +0530

    Fixed resume 404 - part 4

--- next.config.js
+++ next.config.js
@@ -4,19 +4,4 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = {
-  nextConfig,
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      },
-    });
-    return config;
-  }
-}
+module.exports = nextConfig
