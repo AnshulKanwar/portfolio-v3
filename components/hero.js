da57c9d0741f2508eecc5ad6dcfa020fa6c62036
Anshul Kanwar <4n5hu1@protonmail.com>
2022-08-19 09:47:36 +0530

    Fixed resume 404 - part 6

--- components/hero.js
+++ components/hero.js
@@ -54,11 +54,11 @@ const Hero = () => {
                 Let&#39;s Talk
               </button>
             </Link>
-            {/* <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+            <Link href="/resume.pdf" download target="_blank" rel="noopener noreferrer">
               <button className="px-5 py-2 rounded-md bg-indigo-600 hover:bg-indigo-700 text-white">
                 Resume
               </button>
-            </a> */}
+            </Link>
           </div>
         </div>
       </div>
